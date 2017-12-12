6aa43633b3361c6aeb06b7b38715db1bdf45285e
LoOTW <loic.ds1@hotmail.fr>
2017-12-12 09:09:56 +0100

    AnswerComponent

--- client/src/components/answer.tsx
+++ client/src/components/answer.tsx
@@ -1,29 +1,32 @@
-import * as React from 'react'
+import * as React from "react";
 
 interface AnswerProps {
-  userIcon: string,
-  userNickName: string,
-  answerText: string
+  userIcon: string;
+  userNickName: string;
+  answerText: string;
 }
 
-const answer = (props: AnswerProps) => {
-  return (
-    <div className="answer">
-      <div className="answer_gutter">
-        <div className="user_icon"> {props.userIcon} </div>
-      </div>
-      <div className="answer_content">
-        <div className="answer_header">{props.userNickName}</div>
-        <div className="answer_text">
-          {props.answerText}
+class AnswerComponent extends React.Component<AnswerProps> {
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="answer">
+        <div className="answer_gutter">
+          <div className="user_icon"> {this.props.userIcon} </div>
+        </div>
+        <div className="answer_content">
+          <div className="answer_header">{this.props.userNickName}</div>
+          <div className="answer_text">{this.props.answerText}</div>
+        </div>
+        <div className="message_buttons">
+          <span className="message_buttons_points">3</span>
+          <span className="message_buttons_addPoints">+1</span>
         </div>
       </div>
-      <div className="message_buttons">
-        <span className="message_buttons_points">3</span>
-        <span className="message_buttons_addPoints">+1</span>
-      </div>
-    </div>    
-  )
+    );
+  }
 }
-
-export default answer
+export default AnswerComponent;
